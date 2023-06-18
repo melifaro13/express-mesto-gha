cc963a359d8d48ca06376804dfb14cac7ce6072a
Andrey Aleksandrov <melifaro13@yandex.ru>
2023-06-18 21:03:07 +0300

    feat: 14 Проектная работа

--- controllers/users.js
+++ controllers/users.js
@@ -30,31 +30,14 @@ const getUser = (req, res, next) => {
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name,
-    about,
-    avatar,
-    email,
-    password,
-  } = req.body;
-
+  const { name, about, avatar, email, password } = req.body;
   User.findOne({ email })
-    .then((existingUser) => {
-      if (existingUser) {
-        throw new ConflictError(
-          'Пользователь с таким email уже зарегистрирован',
-        );
+    .then((user) => {
+      if (user) {
+        throw new ConflictError('Пользователь с таким email уже зарегистрирован');
       }
-
-      bcrypt
-        .hash(password, 10)
-        .then((hash) => User.create({
-          name,
-          about,
-          avatar,
-          email,
-          password: hash,
-        }))
+      bcrypt.hash(password, 10)
+        .then((hash) => User.create({ name, about, avatar, email, password: hash }))
         .then((user) => {
           res.send(user);
         })
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,32 +1,57 @@
 const jwt = require('jsonwebtoken');
-
-const handleAuthError = (res) => {
-  res
-    .status(401)
-    .send({ message: 'Необходима авторизация' });
-};
-
-const extractBearerToken = (header) => {
-  return header.replace('Bearer ', '');
-};
+const AuthError = require('../errors/AuthError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    throw new AuthError('Необходима авторизация');
   }
 
-  const token = extractBearerToken(authorization);
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return handleAuthError(res);
+    throw new AuthError('Необходима авторизация');
   }
 
   req.user = payload;
-
   next();
 };
+
+
+
+// const jwt = require('jsonwebtoken');
+
+// const handleAuthError = (res) => {
+//   res
+//     .status(401)
+//     .send({ message: 'Необходима авторизация' });
+// };
+
+// const extractBearerToken = (header) => {
+//   return header.replace('Bearer ', '');
+// };
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     return handleAuthError(res);
+//   }
+
+//   const token = extractBearerToken(authorization);
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, 'super-strong-secret');
+//   } catch (err) {
+//     return handleAuthError(res);
+//   }
+
+//   req.user = payload;
+
+//   next();
+// };
