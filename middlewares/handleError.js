2f7b6613f67be5dec88894580893560ee8ea15ad
Andrey Aleksandrov <melifaro13@yandex.ru>
2023-06-20 19:44:56 +0300

    fix: Исправлены ошибки в Проектной работе 14

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,7 +1,7 @@
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
-    message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
   next();
 };
